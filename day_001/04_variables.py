a4ab4eeef36ebf690b7163169ccc4b6d932715c0
Cuong Tran <emsignouc0407@gmail.com>
2024-10-10 16:04:03 +0700

    Complete Day 1

---
+++ day_001/04_variables.py
@@ -0,0 +1,11 @@
+name = "Tom"
+print(name)
+
+name = "Cuong"
+print(name)
+
+# print(len(input("What is your name?")))
+
+username = input("What is your name?: ")
+length = len(username)
+print(length)
---
+++ day_001/05_variable_naming.py
@@ -0,0 +1,3 @@
+name = "Cuong"
+length = len(name)
+print(length)
